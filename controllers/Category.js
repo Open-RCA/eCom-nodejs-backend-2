f002871afb0bc6ce333f2e4bed23cdf278fed638
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-06 20:00:56 +0200

    feature: category done

--- controllers/Category.js
+++ controllers/Category.js
@@ -1,3 +1,48 @@
+const Category = require("../models/Category")
+
 exports.getAllCategories = async (req, res) => {
-  res.status(200).json({ msg: "Hello from categories" })
+  try {
+    const categories = await Category.find()
+    res.status(200).json({ success: true, data: categories })
+  } catch (error) {
+    res.status(400).json({ success: false, data: [] })
+  }
+}
+
+exports.getCategory = async (req, res) => {
+  try {
+    const category = await Category.findOne({ _id: req.params.id })
+    res.status(200).json({ success: true, data: category })
+  } catch (error) {
+    res.status(400).json({ success: false, data: [] })
+  }
+}
+
+exports.updateCategory = async (req, res) => {
+  try {
+    let category = await Category.findById(req.params.id)
+
+    if (!category) {
+      res
+        .status(400)
+        .json({ success: true, msg: "Category not found", data: [] })
+    }
+
+    category = await Category.findOneAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    res.status(200).json({ success: true, data: category })
+  } catch (error) {
+    res.status(400).json({ success: false, data: [] })
+  }
+}
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    await Category.findOneAndRemove({ _id: req.params.id })
+    res.status(200).json({ success: true, data: [] })
+  } catch (error) {
+    res.status(400).json({ success: false, data: [] })
+  }
 }
