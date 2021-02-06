1bf1abe963dba70eae4af6c79c5d533a26750f84
Hagenimana Yassin <hyassin509@gmail.com>
2021-02-06 17:14:58 +0200

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require("express")
 const app = express()
-const PORT = 3000
+const bodyParser = require("body-parser")
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+const PORT = 6000
+const connectDB = require('./config/db')
+dotenv.config({ path: './config/config.env'})
+connectDB()
+
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-  res.json({ msg: "Hello from e-commerce" })
+  res.json({ Message: "Hello from e-commerce" })
 })
 
 const product = require("./routes/product")
+const User=require("./routes/User")
 
 app.use("/product", product)
+app.use("/User",User)
 
 app.listen(PORT, () => {
   console.log(`Server has started at ${PORT}`)
